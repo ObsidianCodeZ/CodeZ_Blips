60445c04eae29509f28e6de0bd5da3822d55bb5b
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-13 16:29:42 +0200

    Add files via upload

---
+++ codez_blips/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+name "CodeZ_Blips"
+author "Obsidian_CodeZ"
+description 'Blips Script By Obsidian_CodeZ'
+lua54 "yes"
+
+client_scripts {
+    "client.lua",
+}
+
+server_scripts {
+    "server.lua",
+}
+
+shared_scripts { 
+    'config.lua',
+}

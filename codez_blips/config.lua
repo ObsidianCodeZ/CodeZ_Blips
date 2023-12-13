41d9549a8d28773ae3148337b7de8e501b3d9512
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-13 17:19:25 +0200

    Update config.lua

--- codez_blips/config.lua
+++ codez_blips/config.lua
@@ -2,18 +2,21 @@ Config = {}
 
 Config.Blips = {
 { Label = "Test1",  -- name to display on the map
-  colour = 46,      -- for  colour codes (https://wiki.gtanet.work/index.php?title=Blips)
-  id = 16,          -- for blip index (https://wiki.gtanet.work/index.php?title=Blips)
+  id = 431,         -- for blip index (https://wiki.gtanet.work/index.php?title=Blips)
+  DisplaySize = 4,  -- DisplaySize 
+  ScaleSize = 0.8,  -- ScaleSize
+  colour = 34,      -- for  colour codes (https://wiki.gtanet.work/index.php?title=Blips)
   x = 191.1,        -- vector3 x
   y = -1019.79,     -- vector3 y
   z = 29.34         -- vector3 z
 },
 -- { Label = "Test2",  -- name to display on the map
---   colour = 46,      -- for  colour codes (https://wiki.gtanet.work/index.php?title=Blips)
---   id = 16,          -- for blip index (https://wiki.gtanet.work/index.php?title=Blips)
---   x = 239.73,       -- vector3 x
---   y = -1035.99,     -- vector3 y
---   z = 29.31         -- vector3 z
+--   id = 431,         -- for blip index (https://wiki.gtanet.work/index.php?title=Blips)
+--   DisplaySize = 4,  -- DisplaySize 
+--   ScaleSize = 0.8,  -- ScaleSize
+--   colour = 34,      -- for  colour codes (https://wiki.gtanet.work/index.php?title=Blips)
+--   x = 191.1,        -- vector3 x
+--   y = -1019.79,     -- vector3 y
+--   z = 29.34         -- vector3 z
 -- },
-
 }

2cd84cf16480debef0a81badc0c876789d5f120c
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-19 01:47:48 +0200

    Update fxmanifest.lua

--- codez_blips/fxmanifest.lua
+++ codez_blips/fxmanifest.lua
@@ -3,6 +3,7 @@ game 'gta5'
 name "CodeZ_Blips"
 author "Obsidian_CodeZ"
 description 'Blips Script By Obsidian_CodeZ'
+contact 'https://discord.com/invite/8nQcNAYTju'
 lua54 "yes"
 
 client_scripts {
